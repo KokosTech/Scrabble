d3451bb0a7a4d24944d3c9a2ebc6d5262b6e9191
Kaloyan Doychinov <kokolia1070@gmail.com>
2022-06-23 14:49:23 +0300

    Revert "[FIX] Aleko"
    This reverts commit 811eaefdb02f6f2b87cbec1ff14d22bc68f493d2.

--- src/ptrie/ptrie_node.hpp
+++ src/ptrie/ptrie_node.hpp
@@ -25,8 +25,6 @@ class TrieNode {
         void setIsEnd(bool isEnd);
         bool getIsEnd() const;
         TrieEdge *getEdge(char firstCharacter) const;
-        const std::unordered_map<char, TrieEdge*> &getEdges() const;
-        unsigned int getEdgeCount() const;
 };  
 
 void addEdgeToTrieNode(TrieNode *node, char firstCharacter, const std::string &prefix, bool edgeNodeIsEnd);
