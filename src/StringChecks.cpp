517253c29e1acb0b76f6c8fd74bdccd9f340ffd2
Moiseicho <mateizahariev@gmail.com>
2022-06-25 16:22:23 +0300

    Finished string checks

--- src/StringChecks.cpp
+++ src/StringChecks.cpp
@@ -35,14 +35,17 @@ bool StringChecks::PosCheck(std::string str, size_t x, size_t y, char dir)
     return 0;
 }
 
-bool StringChecks::BoardCheck(std::string str, size_t x, size_t y, char dir, char board[15][15], std::map<char, int> letters)
+bool StringChecks::BoardCheck(std::string str, size_t x, size_t y, char dir, char board[15][15], std::map<char, int> letters, bool first)
 {
+    if(!PosCheck(str, x, y, dir))return 0;
+    //Check if str is a word from the tree
     if(dir == 'H')
     {
         bool OtherLet = 0;
         std::string neededLet = str;
 
-        if(board[x-1][y] != '*')
+        if(x > 0 && board[x-1][y] != '*')return 0;
+        if(x + str.size() < 15 && board[x + str.size()][y] != '*')return 0;
         for(int i = 0; i < str.size(); i++)
         {
             if(board[x+i][y] != '*')
@@ -65,41 +68,52 @@ bool StringChecks::BoardCheck(std::string str, size_t x, size_t y, char dir, cha
                     temp += board[x+i][z];
                     OtherLet = 1;
                 }
-                if(temp.size > 1)
+                if(temp.size() > 1)
                 {
                     //check if Temp is a real word;
                 }
             }
         }
+        if(!OtherLet && !first)return 0;
+        if(!StringChecks::LetCheck(neededLet, letters))return 0;
+    }
 
-        for(; board[x-1][y] != '*' && x > 0; x--)
-        {
-            str = board[x-1][y] + str;
-        }
+    if(dir == 'V')
+    {
+        bool OtherLet = 0;
+        std::string neededLet = str;
 
-        //Check if word is a word in tree
+        if(y > 0 && board[x][y - 1] != '*')return 0;
+        if(y + str.size() < 15 && board[x][y + str.size()] != '*')return 0;
         for(int i = 0; i < str.size(); i++)
         {
-            if(board[x+i][y] != '*')
+            if(board[x][y+i] != '*')
             {
-                std::map<char, int>::iterator itr;
-                bool Nfound = 1;
-                for(itr = letters.begin(); itr != letters.end(); itr++)
+                if(board[x][y+i] != str[i]) return 0;
+
+                neededLet.erase(i, 1);
+                OtherLet = 1;
+            }else
+            {
+                std::string temp = "" + board[x][y+i];
+                for(int z = x + 1; board[z][y+i] != '*' && z < 15; z++)
                 {
-                    if(str[i] == itr->first)
-                    {
-                        itr->second++;
-                        Nfound = 0;
-                        break;
-                    }
+                    temp += board[z][y+i];
+                    OtherLet = 1;
                 }
-                if(Nfound)
+
+                for(int z = x - 1; board[z][y+i] != '*' && z >= 0; z--)
                 {
-                    letters.insert(std::pair<char, int>(board[x+i][y], 1));
+                    temp += board[z][y+i];
+                    OtherLet = 1;
+                }
+                if(temp.size() > 1)
+                {
+                    //check if Temp is a real word;
                 }
             }
         }
-        StringChecks::LetCheck(str, letters);
-
+        if(!OtherLet && !first)return 0;
+        if(!StringChecks::LetCheck(neededLet, letters))return 0;
     }
 }
--- src/StringChecks.hpp
+++ src/StringChecks.hpp
@@ -9,5 +9,5 @@ public:
 
     static bool LetCheck(std::string str, std::map<char, int> letters);
     static bool PosCheck(std::string str, size_t x, size_t y, char dir);
-    static bool BoardCheck(std::string str, size_t x, size_t y, char dir, char board[15][15], std::map<char, int> letters);
+    static bool BoardCheck(std::string str, size_t x, size_t y, char dir, char board[15][15], std::map<char, int> letters, bool first);
 };
